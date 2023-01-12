4199c7621ab0299af658817b101923ddb6c869bd
Seba <sebase.pereyra@gmail.com>
2023-01-11 23:42:08 -0300

    Save data in localStorage

--- src/App/index.js
+++ src/App/index.js
@@ -9,8 +9,18 @@ const defaultTodos = [
 
 function App() {
 
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos = [];
+  if(!localStorageTodos){
+    localStorage.setItem('TODOS_V1', JSON.stringify(parsedTodos));
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('');
-  const [todos, setTodos] = React.useState(defaultTodos);
+
   const completedTodos = todos.filter(todo => todo.completed).length;
   const totalTodos = todos.length;
 
@@ -26,18 +36,24 @@ function App() {
     })
   }
 
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos);    
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+    setTodos(newTodos);
+  }
+
   const completeTodo = (todoKey) => {
     const todoIndex = todos.findIndex(todo => todo.text === todoKey);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const deleteTodo = (todoKey) => {
     const todoIndex = todos.findIndex(todo => todo.text === todoKey);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   return (
