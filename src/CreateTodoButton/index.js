39ab0656fa8f9e53719b6d097c53a9ae5e26e966
Seba <sebase.pereyra@gmail.com>
2023-01-12 22:12:42 -0300

    Toggle modal

--- src/CreateTodoButton/index.js
+++ src/CreateTodoButton/index.js
@@ -4,7 +4,7 @@ import './CreateTodoButton.css'
 function CreateTodoButton(props) {
 
     const onClickButton = () => {
-        props.setOpenModal(true);
+        props.setOpenModal(prevState => !prevState);
     }
     return (
         <button
